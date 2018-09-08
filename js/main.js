01180449d091b73bc7a5ce687883198bd30ca171
VaisakhPradeep <92vaisakh92@gmail.com>
2018-09-08 14:43:43 +0530

    responsiveness in progress

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+
+	$(".menu-icon").click(function(){
+		$(".nav-list").toggleClass("selected");
+	});
+
+	$(".nav-list").click(function(){
+		$(this).toggleClass("selected");
+	});
+	
 	$(document).on('click', '.nav', function(event){
     event.preventDefault();
 
